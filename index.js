af524a0c9cf52c7c9ea387e8434d9caadb976d2e
Алсу Исламова <161420770+mrrled@users.noreply.github.com>
2026-03-03 18:08:09 +0500

    add user command

--- index.js
+++ index.js
@@ -12,12 +12,17 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    const args = command.split(' ')[1];
+    const com = command.split(' ')[0];
+    switch (com) {
         case 'exit':
             process.exit(0);
             break;
         case 'important':
             console.log(important());
+            break;
+        case 'user':
+            console.log(user(args));
             break;
         default:
             console.log('wrong command');
@@ -50,4 +55,33 @@ function important() {
         }
     }
     return result;
+}
+
+function user(username) {
+    const lowerUsername = username.toLowerCase();
+    const nameMatch = getNameMatch();
+    const ans = nameMatch.get(lowerUsername);
+    if (ans === undefined) {
+        return 'Not found comments for this username';
+    }
+    return ans;
+}
+
+function getNameMatch(){
+    const lines = show();
+    const nameMatch = new Map();
+    for (const line of lines) {
+        let parts = line.split(';');
+        if (parts.length !== 3) {
+            continue;
+        }
+        const name = parts[0].split(' ')[2].toLowerCase();
+        if (nameMatch.has(name)) {
+            const arr = nameMatch.get(name);
+            arr.push(line);
+        }else{
+            nameMatch.set(name, [line]);
+        }
+    }
+    return nameMatch;
 }
